302b5ac2281cc9106edbcedb9969930f1d9baab1
VISHNUKUMAR M <vk0471318@gmail.com>
2026-08-24 09:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ Rules:
             try:
 
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                  model="gemini-3.6-flash",
                     contents=[
                         types.Part.from_bytes(
                             data=image_file.getvalue(),
